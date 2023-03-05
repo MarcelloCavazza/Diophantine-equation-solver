9cbd3eae2dae38ca1adc5157ed2b36e898d575dd
Marcello Cavazza <63473199+MarcelloCavazza@users.noreply.github.com>
2023-03-04 22:52:29 -0300

    classe mdc

---
+++ Diophantine-equation-solver/MDCResults.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diophantine_equation_solver
+{
+    internal class MDCResults : IMDCREsults
+    {
+        public MDCResults(int rValue, int aValue, int bValue, int qValue) {
+            this.A = aValue;
+            this.B = bValue;
+            this.R = rValue;
+            this.Q = qValue;
+        } 
+        public int R { get; set; }
+        public int A { get; set; }
+        public int B { get; set; }
+        public int Q { get; set; }
+    }
+}
