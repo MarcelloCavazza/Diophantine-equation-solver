919f7e481b10825fccc5e5e01f3d18677c2e73e9
Marcello Cavazza <63473199+MarcelloCavazza@users.noreply.github.com>
2023-03-04 22:52:21 -0300

    fazendo interface

---
+++ Diophantine-equation-solver/IMDCREsults.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diophantine_equation_solver
+{
+    internal interface IMDCREsults
+    {
+        public int R { get; set; }
+        public int A { get; set; }
+        public int B { get; set; }
+        public int Q { get; set; }
+    }
+}
